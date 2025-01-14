2491561ff8f705d3038839f0fdeb85c67aaa12e6
1seyoung <akdlrnal0705@gmail.com>
2025-01-14 16:36:58 +0900

    Initial commit

---
+++ src/main/java/com/gatheria/GatheriaApplication.java
@@ -0,0 +1,13 @@
+package com.gatheria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatheriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gatheria/GatheriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatheria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
