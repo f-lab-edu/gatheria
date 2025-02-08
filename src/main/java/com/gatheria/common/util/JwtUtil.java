7606e454679fa61ce17f145f6b7a6a45e00e8838
1seyoung <akdlrnal0705@gmail.com>
2025-02-09 05:44:16 +0900

    feat : JwtUtil 기능 구현 - token 정보에 memberid 추가 - 토큰 검증 메소드 구현 - 토큰에서 정보 parsing (extractEmail, extractRole, extractMemberId)

--- src/main/java/com/gatheria/common/util/JwtUtil.java
+++ src/main/java/com/gatheria/common/util/JwtUtil.java
@@ -2,6 +2,7 @@ package com.gatheria.common.util;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
@@ -21,18 +22,49 @@ public class JwtUtil {
         return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
-    public String createAccessToken(String email, String role) {
+    public String createAccessToken(String email, String role, Long memberId) {
+
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationTime);
+
         return Jwts.builder()
                 .setSubject(email)
                 .claim("role", role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .setIssuedAt(now)
+                .setExpiration(expiration)
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public boolean validateAccessToken(String token) {
-        //TODO :  Access Token 검증 로직
-        return true;
+        Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJwt(token);
+
+        Claims claims = extractClaims(token);
+        Date expiration = claims.getExpiration();
+        return !expiration.before(new Date());
     }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractClaims(token).get("role", String.class);
+    }
+
+    public Long extractMemberId(String token) {
+        return extractClaims(token).get("memberId", Long.class);
+    }
+
 }
